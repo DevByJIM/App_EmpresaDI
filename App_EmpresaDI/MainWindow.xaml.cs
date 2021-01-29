f5076431ff92556d6437435c64f5094f33ebff66
DevByJIM <70746542+DevByJIM@users.noreply.github.com>
2021-01-29 09:39:26 +0100

    Inicio

--- App_EmpresaDI/MainWindow.xaml.cs
+++ App_EmpresaDI/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,45 @@ namespace App_EmpresaDI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TrabajarBotones(object sender, RoutedEventArgs e)
+        {
+            BotonesAlternar(sender);
+            switch (((ToggleButton)sender).Name)
+            {
+                case "btn_Home":
+                    //fr_Paginas.Navigate(new pg_Home());
+                    break;
+                case "btn_Clientes":
+                    //fr_Paginas.Navigate(new pg_Clientes());
+                    break;
+                case "btn_Productos":
+                    //fr_Paginas.Navigate(new pg_Productos());
+                    break;
+                case "btn_Ventas":
+                    //fr_Paginas.Navigate(new pg_Ventas());
+                    break;
+            }
+        }
+
+        private void BotonesAlternar(object sender)
+        {
+            foreach (ToggleButton btn in grid_Botones.Children)
+            {
+                btn.IsChecked = false;
+            }
+            ((ToggleButton)sender).IsChecked = true;
+        }
+
+        private void AjusteFuentes(object sender, SizeChangedEventArgs e)
+        {
+            lbTitulo.FontSize = this.ActualHeight * 0.05;
+
+            foreach (ToggleButton btn in grid_Botones.Children)
+            {
+                btn.FontSize = this.ActualHeight * 0.02;
+            }
         }
     }
 }
